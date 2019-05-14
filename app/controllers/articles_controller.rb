b9be416c78e97c00d90c1efb31fbbf96e77ccb36
bschw <bschwarz890@gmail.com>
2019-05-13 20:01:30 -0700

    wire up quill editor to create action, can now save blog post to db!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
 class ArticlesController < ApplicationController
 	def index
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		Article.create(article_params)
+	end
+
+	def article_params
+		params.require(:article).permit(:description, :title)
+	end
 end
