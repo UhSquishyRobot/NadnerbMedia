051aee6e0e3ec5cb4cc5a62417a492ec61944e0d
bschw <bschwarz890@gmail.com>
2019-05-19 13:12:11 -0700

    add delete and begin edit on articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		Article.create(article_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		Article.destroy(params[:id])
+		redirect_to root_path
 	end
 
 	def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get '/articles/new', to: 'articles#new'
-  post '/articles', to: 'articles#create'
+  # get '/articles/new', to: 'articles#new'
+  # post '/articles', to: 'articles#create'
+  resources :articles
 end
